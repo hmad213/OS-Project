0162f630f6281734a15be362c75b6f07290c2786
Alyan Ahmed <alyanahmed969@gmail.com>
2026-05-07 17:48:00 +0500

    add definition for batch Init and destroy functions

--- src/pipeline.c
+++ src/pipeline.c
@@ -1,1 +1,22 @@
 #include "../include/pipeline.h"
+#include <stdlib.h>
+
+#define BUFFER_CAPACITY 10
+
+void batchInit(Batch* b, int size){
+    b->count = size;
+    b->paths = (char**)malloc(size * sizeof(char*));
+    for(int i = 0; i < size; i++){
+        b->paths[i] = NULL; 
+    }
+}
+
+void batchDestroy(Batch* b){
+    if(b->paths != NULL){
+        
+        free(b->paths);
+        b->paths = NULL;
+    }
+    b->count = 0;
+}
+
